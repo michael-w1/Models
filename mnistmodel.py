f6eecea68af9764ceb27e0b672fa6e06d2829eb5
carbon56 <55727701+michael-w1@users.noreply.github.com>
2019-12-01 20:46:56 -0700

    Added relu layers, softmax layer. Epoch = 15

--- mnistmodel.py
+++ mnistmodel.py
@@ -26,12 +26,19 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 print("--Make model--")
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(10, activation='sigmoid')
+  tf.keras.layers.Dense(256, activation='relu'),
+  tf.keras.layers.Dense(100, activation='relu'),  
+  tf.keras.layers.Dense(10, activation='softmax')
+
+  
 ])
+
+
+
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 print("--Fit model--")
-model.fit(x_train, y_train, epochs=3, verbose=2)
+model.fit(x_train, y_train, epochs=15, verbose=2)
 
 print("--Evaluate model--")
 model_loss, model_acc = model.evaluate(x_test,  y_test, verbose=2)
